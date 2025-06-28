18e174405c4313695a571388fcf7d9496b38519a
FCJar <Fcjardim764@gmail.com>
2025-06-27 23:12:49 -0300

    fix:alteração verifica usuario

--- src/Middlewares/verificarUsuario.js
+++ src/Middlewares/verificarUsuario.js
@@ -2,8 +2,8 @@ function verificarUsuario(req, res, next){
     const usuarioId = req.params.id || req.params.id_usuario ||req.body.id_usuario;
     if(req.usuarioId !== usuarioId){
         return res.status(401).json("Operação não Autorizada");
+        console.log({ usuarioId: req.usuarioID });
     }
-    //console.log({ usuarioId: req.usuarioID });
     next();
 }
 
